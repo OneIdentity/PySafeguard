5b329184ec5a44ac0c2d047f3de23230f5a16255
Eddie Mun <edward.mun@quest.com>
2022-09-27 14:52:56 -0700

    A2a updates

--- src/pysafeguard.py
+++ src/pysafeguard.py
@@ -17,14 +17,14 @@ class HttpMethods:
     PUT = requests.put
     DELETE = requests.delete
 
-class A2ATypes(Enum):
-    PASSWORD = 1
-    PRIVATEKEY = 2
+class A2ATypes:
+    PASSWORD = "password"
+    PRIVATEKEY = "privatekey"
 
-class SshKeyFormats(Enum):
-    OPENSSH = 1
-    SSH2 = 2
-    PUTTY = 3
+class SshKeyFormats:
+    OPENSSH = "openssh"
+    SSH2 = "ssh2"
+    PUTTY = "putty"
 
 def _assemble_path(*args):
     return '/'.join(map(lambda x: str(x).strip('/'), args))
@@ -78,14 +78,28 @@ class PySafeguardConnection:
         else:
             raise Exception('{} {}: {} {}\n{}'.format(req.status_code,req.reason,req.request.method,req.url,req.text))
 
-    def connect_certificate(self, cert_file, key_file, pfx_file, passphrase, provider='certificate'):
-        # TODO: rSTS logic integration to get an access token
-        self.access_token = None
+    def connect_certificate(self, provider='certificate'):
+        body = {
+          'scope': 'rsts:sts:primaryproviderid:{}'.format(provider),
+          'grant_type': 'client_credentials'
+        }
+        req = self.__execute_web_request(HttpMethods.POST,Services.RSTS,'oauth2/token',{},body,{})
+        if req.status_code == 200 and 'application/json' in req.headers.get('Content-type',''):
+            data = req.json()
+            req = self.__execute_web_request(HttpMethods.POST,Services.CORE,'Token/LoginResponse',{},dict(StsAccessToken=data.get('access_token')),{})
+            if req.status_code == 200 and 'application/json' in req.headers.get('Content-type',''):
+                data = req.json()
+                self.UserToken = data.get('UserToken')
+                self.headers.update(Authorization='Bearer {}'.format(self.UserToken))
+            else:
+                raise Exception('{} {}: {} {}\n{}'.format(req.status_code,req.reason,req.request.method,req.url,req.text))
+        else:
+            raise Exception('{} {}: {} {}\n{}'.format(req.status_code,req.reason,req.request.method,req.url,req.text))
 
     def invoke(self, httpMethod, httpService, endpoint=None, query={}, body=None, additionalHeaders={}):
         return self.__execute_web_request(httpMethod, httpService, endpoint, query, body, additionalHeaders)
 
-    def a2a_get_credential(self, apiKey, type, keyFormat, cert, key, passphrase):
+    def a2a_get_credential(self, apiKey, type, keyFormat):
         #TODO: get the a2a credential
         # Example:
         # let credential = await SafeguardJs._executePromise(`https://${hostName}/service/a2a/v3/Credentials?type=${type}&keyFormat=${keyFormat}`, SafeguardJs.HttpMethods.GET, null, 'json', additionalHeaders, null, null, httpsAgent);
@@ -95,39 +109,20 @@ class PySafeguardConnection:
         if type == None or type == "":
             raise Exception("type may not be null or empty")
 
-        if cert == None or cert == "" or key == None or key == "":
-            raise Exception("A cert and key must be specified")
-
-        if passphrase == None or passphrase == "":
-            raise Exception("A passphrase must be specified")
-
         if keyFormat == None or keyFormat == "":
             keyFormat = SshKeyFormats.OPENSSH
 
-
-    def a2a_get_credential_from_files(self, apiKey, type, keyFormat, certFile, keyFile, passphrase):
-        '''(Public) Retrieves an application to application credential.
-        * @param {string}              hostName    (Required) The name or ip of the safeguard appliance.
-        * @param {string}              apiKey      (Required) The a2a api key.
-        * @param {string}              type        (Required) The type of credential to retrieve (password, privatekey, etc).
-        * @param {string}              keyFormat   (Optional) The privateKeyFormat to return (openssh, ssh2, putty, etc).
-        * @param {string}              certFile    (Required) The user certificate file location in pem format.
-        * @param {string}              keyFile     (Required) The user certificate's key file location in key format.
-        * @param {string}              passphrase  (Required) The user certificate's passphrase.
-        '''
-        cert = None
-        key = None
-
-        if certFile == None or certFile == "" or keyFile == None or keyFile == "":
-            raise Exception("cert path and key path must be specified.")
-        else:
-            f = open(certFile, 'r')
-            cert = f.read()
-
-            f = open(keyFile, 'r')
-            key = f.read()
-            
-        return self.a2a_get_credential(apiKey, type, keyFormat, cert, key, passphrase)
+        try:
+            header = {
+                'authorization': 'A2A {}'.format(apiKey)
+            }
+            query = {
+                'type': type,
+                'keyFormat': keyFormat
+            }
+            return self.__execute_web_request(HttpMethods.GET, Services.A2A, endpoint="Credentials", query=query, body={}, headers=header)
+        except:
+            raise Exception("Failed to retrieve credentials")
 
     def register_signalr(self, callback):
         #TODO: register the signalr callback
