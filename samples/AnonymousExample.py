a32d6c886d823bc74bb686968b8c67d7b41a497b
Eddie Mun <edward.mun@quest.com>
2022-09-28 11:44:01 -0700

    added .py extension

---
+++ samples/AnonymousExample.py
@@ -0,0 +1,11 @@
+from pysafeguard import *
+import json
+
+hostName = ""
+
+print('Connecting anonymously')
+connection = PySafeguardConnection(hostName, False)
+
+print('Getting status')
+result = connection.invoke(HttpMethods.GET, Services.NOTIFICATION, 'Status')
+print(json.dumps(result.json(),indent=2,sort_keys=True))
