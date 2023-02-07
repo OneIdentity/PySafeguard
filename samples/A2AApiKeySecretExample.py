2f50b7dc63d2fc8b8a63c18c1cb39397486fea35
petrsnd <petrsnd@gmail.com>
2023-02-07 11:32:11 -0700

    Correct coding errors in sample

--- samples/A2AApiKeySecretExample.py
+++ samples/A2AApiKeySecretExample.py
@@ -19,12 +19,9 @@ userKeyFile = ''
 
 print('Retrieving api key secret credentials')
 
-apiKeys_json = PySafeguardConnection.a2a_get_credential(hostName, apiKey, userCertFile, userKeyFile, caFile, A2ATypes.APIKEYSECRET)
-print(f'API Key JSON: {apiKeys_json}')
-
-apiKeys = json.loads(apiKeys_json)
+apiKeys = PySafeguardConnection.a2a_get_credential(hostName, apiKey, userCertFile, userKeyFile, caFile, A2ATypes.APIKEYSECRET)
+print(f'API Key JSON: {apiKeys}')
 print(f'API Keys Count: {len(apiKeys)}')
-
 for apiKey in apiKeys:
-    printf(f'Client Id: {apiKey.ClientId}')
-    printf(f'Client Secret: {apiKey.ClientSecret}')
+    print(f'Client Id: {apiKey["ClientId"]}')
+    print(f'Client Secret: {apiKey["ClientSecret"]}')
